df11c9dbb7d81e6db1b8bd0c3bb22db22c2a71b9
Shamal Siriwardana <shamal.v.siriwardana@gmail.com>
2024-03-04 21:33:28 -0500

    [SS] Main class and method for the project

---
+++ programming_skills/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        BasicImplementation basicImplementation = new BasicImplementation();
+        basicImplementation.mergeStringAlternately("me" , "hi");
+        basicImplementation.findTheDifference("b", "bb");
+    }
+}
